10ac74d2f4184aecd02822909c0eb79061e3b2dd
Anastasiia Boiko <nastasyah86@gmail.com>
2021-08-01 14:47:21 +0300

    /

--- js_beginning/multiplicationTable.js
+++ js_beginning/multiplicationTable.js
@@ -1,10 +1,9 @@
 const multiplicationTable = size => {
   let mainArray = [];
-  for (let i = 0; i < size; i++) {
-    let row = [];
-    for (let j = 0; j < size; j++) {
-      row.push[j];
-    }
+  for (let i = 1; i <= size; i++) {
+    let row = Array(size)
+      .fill(1)
+      .map((el, index) => (el + index) * i);
     mainArray.push(row);
   }
   return mainArray;
