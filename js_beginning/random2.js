7396d611249c2362044b58b81da620b54a74f7aa
Anastasiia Boiko <nastasyah86@gmail.com>
2021-08-08 12:22:22 +0300

    object create

--- js_beginning/random2.js
+++ js_beginning/random2.js
@@ -16,7 +16,21 @@
 // f();
 // console.log(null >= 0);
 // console.log(null == null);
-const g = (w = a = ~~2.1337);
-const s = x => y => z => (x << y) << z;
+// const g = (w = a = ~~2.1337);
+// const s = x => y => z => (x << y) << z;
 
-console.log(s(w)(a)(g));
+// console.log(s(w)(a)(g));
+
+// var arr = [0, 1, 2, 3];
+// console.log(arr.reduce());
+
+// console.log((1, 5 - 1) * 2);
+// console.log(1, 5 - 1);
+
+var Employee = {
+  company: 'xyz',
+};
+var emp1 = Object.create(Employee);
+console.log(emp1);
+// delete emp1.company;
+// console.log(emp1.company);
