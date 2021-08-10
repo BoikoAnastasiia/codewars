78577027631458025dc23215c861a85cf698b096
Anastasiia Boiko <nastasyah86@gmail.com>
2021-08-10 19:56:35 +0300

    withParamsCountValidation

--- js_beginning/testTaskforchatbot/index.js
+++ js_beginning/testTaskforchatbot/index.js
@@ -19,3 +19,32 @@
 // get(example, 'a.b.c'); /* [1,2,3] */
 // get(example, 'a.b.c.1'); /* 2 */
 // get(example, ['a', 'b', 'c']); /* [1,2,3] */
+
+/**
+ * Should deep compare objects and ensure all nested fields are equal
+ * @param {object} a
+ * @param {object} b
+ 
+ *
+ * deepEqual(a, b) -> true
+ * deepEqual(a, c) -> false
+ * deepEQual(a, d) -> false
+ */
+
+// const a = { field: { nested: 'value' } };
+// const b = { field: { nested: 'value' } };
+// const c = { field: { nested: 'value1' } };
+// const d = { field: { nested: 'value', other: 'field' } };
+
+// const deepEqual = (a, b) => {
+//   return JSON.stringify(a) == JSON.stringify(b);
+// };
+
+// console.log(deepEqual(a, d));
+
+/**
+ * Should decorate function to add params count validation
+ * @param {number | number[]} count could be a number or [number, number] to specify min and max params count
+ * @param {function} func
+ */
+const withParamsCountValidation = (count, func) => {};
