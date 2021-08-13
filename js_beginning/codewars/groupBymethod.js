13b206efbadfa88c571a0f19a0e1cd1c6a7c4dbf
Anastasiia Boiko <nastasyah86@gmail.com>
2021-08-13 16:10:53 +0300

    without fn

--- js_beginning/codewars/groupBymethod.js
+++ js_beginning/codewars/groupBymethod.js
@@ -1,8 +1,23 @@
 Array.prototype.groupBy = function (fn) {
+  if (!fn) {
+    return this.reduce((acc, item, index, array) => {
+      acc[item] = [...array.filter(el => el === item)];
+      return acc;
+    }, {});
+  }
+
   return this.reduce((acc, item, index, array) => {
-    acc[index] = array.filter(el => el === index);
+    acc[item] = [...array.filter(el => el === item)];
     return acc;
   }, {});
 };
 
 console.log([1, 2, 3, 2, 4, 1, 5, 1, 6].groupBy());
+// {
+//   1: [1, 1, 1],
+//   2: [2, 2],
+//   3: [3],
+//   4: [4],
+//   5: [5],
+//   6: [6]
+// }
