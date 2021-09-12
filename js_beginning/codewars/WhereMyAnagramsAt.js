278e7f8f16936e589c48ef7e285b5d833a122fc5
Anastasiia Boiko <nastasyah86@gmail.com>
2021-09-12 18:06:19 +0300

    done

--- js_beginning/codewars/WhereMyAnagramsAt.js
+++ js_beginning/codewars/WhereMyAnagramsAt.js
@@ -1,13 +1,21 @@
-function anagrams(word, words) {}
-
 const isAnagram = (word1, word2) => {
   const arrayFromWordFirst = word1.split('');
   const arrayFromWordSecond = word2.split('');
 
-  arrayFromWordFirst.reduce(
-    (acc, el, index) => console.log(acc, el, index),
-    arrayFromWordSecond
-  );
+  if (
+    arrayFromWordFirst.reduce((acc, el) => {
+      if (acc.includes(el)) {
+        acc.splice(acc.indexOf(el), 1);
+      }
+      return acc;
+    }, arrayFromWordSecond).length === 0
+  ) {
+    return true;
+  }
 };
 
-console.log(isAnagram('carer', 'racer'));
+function anagrams(word, words) {
+  return words.filter(el => isAnagram(word, el));
+}
+
+console.log(anagrams('racer', ['crazer', 'carer', 'racar', 'caers', 'racer']));
