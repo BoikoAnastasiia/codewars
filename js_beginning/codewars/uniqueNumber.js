2bb84c71f99253031f69aa8a2583f08ac2683b50
Anastasiia Boiko <nastasyah86@gmail.com>
2021-10-31 12:49:01 +0200

    smallest done

--- js_beginning/codewars/uniqueNumber.js
+++ js_beginning/codewars/uniqueNumber.js
@@ -1,5 +1,22 @@
-function findUnique(arr) {
-  return arr.find(value => arr.indexOf(value) === arr.lastIndexOf(value)) || -1;
+function findUnique(A) {
+  var returnNumber = -1;
+  var tempArray = new Array();
+
+  for (i = 0; i < A.length; i++) {
+    var newIndex = A[i];
+    if (tempArray[newIndex] === undefined) {
+      tempArray[newIndex] = 1;
+    } else {
+      tempArray[newIndex] += 1;
+    }
+  }
+  for (j = 0; j < A.length; j++) {
+    if (tempArray[A[j]] == 1) {
+      returnNumber = A[j];
+      break;
+    }
+  }
+  return returnNumber;
 }
 
 console.log(findUnique([1, 8, 4, 4, 6, 1, 8])); //6
